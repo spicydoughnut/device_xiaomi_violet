f6a8ecf03499f13939382ed1d227d785122de585
spicydoughnut <brinchex@gmail.com>
2020-12-29 18:33:03 +0000

    violet: add build fingerprint

--- dot_violet.mk
+++ dot_violet.mk
@@ -26,4 +26,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="flame-user 11 RQ1A.201205.008 6943376 release-keys" \
     PRODUCT_NAME="violet"
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/flame/flame:11/RQ1A.201205.008/6943376:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
